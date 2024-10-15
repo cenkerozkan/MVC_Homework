b07be8695c3fc197136231be05ab8dcdabe79cd2
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2024-10-15 10:00:25 +0300

    INITIAL UPDATE

---
+++ Homework1/BLL/DAL/Student.cs
@@ -0,0 +1,11 @@
+namespace BLL.DAL;
+#nullable disable
+
+public class Students
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime BirthDate { get; set; }
+    public decimal Gpa { get; set; }
+}
